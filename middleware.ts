49d829cf2da53106f98a377c8b41a5021ec25ae8
Raaz <21ketanraaz@gmail.com>
2024-05-12 17:38:22 +0530

    Middle Ware.Ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
+  ignoredRoutes: ["/((?!api|trpc).*)", "/(?!\\.[\\w]+$|_next).*"],
 });
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
